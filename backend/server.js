acec92632164f99c5189d00668405eed68df29c6
Isugithub <isulakshani57@gmail.com>
2026-09-06 20:52:44 +0530

    Prepare backend for Vercel deployment

--- backend/server.js
+++ backend/server.js
@@ -7,15 +7,20 @@ const connectDB = require("./config/db");
 const PORT =
   process.env.PORT || 5000;
 
+  const cors = require("cors");
+app.use(cors());
 
 // Connect MongoDB
 connectDB();
 
 
 // Start server
-app.listen(PORT, () => {
-  console.log(
-    `Server running on port ${PORT}`
-  );
-});
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+// Export the Express app for Vercel
+module.exports = app;
 
